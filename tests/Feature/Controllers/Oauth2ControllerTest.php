8a7561586de2e85f7428d5e2224570d4d3de8ca7
Oluwatobi Omisakin <omisakin.oluwatobi@gmail.com>
2025-04-06 19:16:28 +0300

    Ensure redirect url is required

--- tests/Feature/Controllers/Oauth2ControllerTest.php
+++ tests/Feature/Controllers/Oauth2ControllerTest.php
@@ -12,7 +12,8 @@ class Oauth2ControllerTest extends TestCase
 
         $response = $this->request(
             method: 'get',
-            path: '/authorize?client_id=myawesomeapp'
+            path: '/authorize?'
+            . 'client_id=myawesomeapp'
             . '&redirect_uri=http://examples.test'
             . '&response_type=code'
             . '&state=state'
